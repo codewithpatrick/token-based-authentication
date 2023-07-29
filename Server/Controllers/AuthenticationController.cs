445924bdbafa7c422936db8cbfa28085f0c9cfb5
Patrick Pangilinan <patrick.pangilinan@randemgroup.com>
2023-07-29 20:53:07 +0800

    Register new users using UserManager

--- Server/Controllers/AuthenticationController.cs
+++ Server/Controllers/AuthenticationController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Server.Data;
 using Server.Data.Models;
+using Server.Data.ViewModels;
 
 namespace Server.Controllers;
 
@@ -16,14 +17,48 @@ public class AuthenticationController : ControllerBase
     private readonly UserManager<ApplicationUser> _userManager;
 
     public AuthenticationController(
-        UserManager<ApplicationUser> userManager, 
+        UserManager<ApplicationUser> userManager,
         RoleManager<IdentityRole> roleManager,
-        AppDbContext context, 
+        AppDbContext context,
         IConfiguration configuration)
     {
         _userManager = userManager;
         _roleManager = roleManager;
         _context = context;
         _configuration = configuration;
+    }
+
+    [HttpPost("register-user")]
+    public async Task<IActionResult> Register([FromBody] RegisterVM registerVm)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest("Please, provide all the required fields.");
+        }
+
+        var userExists = await _userManager.FindByEmailAsync(registerVm.EmailAddress);
+
+        if (userExists != null)
+        {
+            return BadRequest($"user {registerVm.EmailAddress} already exists");
+        }
+
+        var newUser = new ApplicationUser()
+        {
+            FirstName = registerVm.FirstName,
+            LastName = registerVm.LastName,
+            Email = registerVm.EmailAddress,
+            UserName = registerVm.UserName,
+            SecurityStamp = Guid.NewGuid().ToString()
+        };
+
+        var result = await _userManager.CreateAsync(newUser, registerVm.Password);
+
+        if (result.Succeeded)
+        {
+            return Ok("User created");
+        }
+
+        return BadRequest("User could not be created");
     }
 }
--- Server/Data/Models/ApplicationUser.cs
+++ Server/Data/Models/ApplicationUser.cs
@@ -6,6 +6,5 @@ namespace Server.Data.Models
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string Custom { get; set; }
     }
 }
