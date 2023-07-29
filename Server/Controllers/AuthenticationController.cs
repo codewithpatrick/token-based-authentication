fc3b417e6ae36ff36eebf52f271ff018fa610b6f
Patrick Pangilinan <patrick.pangilinan@randemgroup.com>
2023-07-29 20:19:09 +0800

    Adding the authentication controller

---
+++ Server/Controllers/AuthenticationController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Server.Data;
+using Server.Data.Models;
+
+namespace Server.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthenticationController : ControllerBase
+{
+    private readonly IConfiguration _configuration;
+
+    private readonly AppDbContext _context;
+    private readonly RoleManager<IdentityRole> _roleManager;
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    public AuthenticationController(
+        UserManager<ApplicationUser> userManager, 
+        RoleManager<IdentityRole> roleManager,
+        AppDbContext context, 
+        IConfiguration configuration)
+    {
+        _userManager = userManager;
+        _roleManager = roleManager;
+        _context = context;
+        _configuration = configuration;
+    }
+}
